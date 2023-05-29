693254bcd077939b3e9d59befd8253f48495e620
Douglas Nunes <dgspai@hotmail.com>
2023-05-28 23:41:30 -0300

    feat: Implements basic producer

---
+++ app/dummy_producer.py
@@ -0,0 +1,22 @@
+import json
+import random
+import time
+
+from google.cloud import pubsub_v1
+
+# Define as informações do projeto e tópico do Pub/Sub
+project_id = "dev-project"
+topic_id = "dev-topic"
+
+
+if __name__ == "__main__":
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+    while True:
+        msg = {
+            "n": random.randint(0, 100)
+        }
+        print(f"Publicando msg: {msg}")
+        publisher.publish(topic_path, json.dumps(msg).encode('utf-8'))
+        time.sleep(1)
+
